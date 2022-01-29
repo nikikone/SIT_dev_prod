96b58abd57f54618d7053b3e378164a01f03b644
nikikone <nikitaz0614@gmail.com>
2022-01-30 04:43:57 +1000

    Deployed development

--- Scripts/deploy_dev.sh
+++ Scripts/deploy_dev.sh
@@ -3,6 +3,6 @@ cd ..
 pip freeze > requirements.txt
 git add .
 git commit -m "Deployed development"
-git push origin
+git push origin main
 
 #ssh -i C:/Users/Nikita/.ssh/id_rsa nikikone@159.89.232.171 'sh ~/server_development/SIT_dev_prod/Scripts/build.sh'
